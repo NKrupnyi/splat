0d0170a2f56f64f1af1d21faced6518949c1aacf
Никита Крупный <krupnyi@iac.spb.ru>
2018-11-25 15:38:33 +0300

    init

---
+++ src/main/java/com/krupnyi/splat/SplatApplication.java
@@ -0,0 +1,12 @@
+package com.krupnyi.splat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplatApplication.class, args);
+    }
+}
---
+++ src/test/java/com/krupnyi/splat/SplatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krupnyi.splat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SplatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
